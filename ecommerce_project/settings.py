18f4072792dc4b8b0158fcb2e3eedb171af8e10a
shabz <shabzcc@gmail.com>
2020-09-04 22:51:04 +0530

    added Procfile and requirements

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3y27q3#j_0=q&ex11_lx$gcu29wh-dvkd5-_uxqy1k07kjd_i='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition

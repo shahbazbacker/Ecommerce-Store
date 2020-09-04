cc5da729ce25ef97ecd33117247174287a55c262
shabz <shabzcc@gmail.com>
2020-09-04 23:04:22 +0530

    added static root

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -139,11 +139,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "ecommapi", "static_cdn", "static_root")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "ecommerce_store", "static_cdn", "static_root")
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "ecommapi", "static_cdn", "media_root")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "ecommerce_store", "static_cdn", "media_root")
 
 
 

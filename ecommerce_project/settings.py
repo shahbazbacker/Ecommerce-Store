1de7b4b1fb6c3e922f31e162aefdd3b5bd233e80
shabz <shabzcc@gmail.com>
2020-09-04 23:47:38 +0530

    removed package resource from req.txt

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -140,11 +140,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "ecommerce_store", "static_cdn", "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "ecommerce_store", "static_cdn", "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
